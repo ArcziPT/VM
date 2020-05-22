3e84fd10e649ff3e6b07fc84c054a98dca3091ea
ArcziPT <awolk@student.agh.edu.pl>
2020-05-22 17:44:09 +0200

    VM comment

--- VM.cpp
+++ VM.cpp
@@ -23,13 +23,17 @@ void VM::run(const std::string& exe_path, mem_add start_add){
 
     bool* exit_flag = config->vmr->get_exit_flag();
     while(*exit_flag != true){
+        //read opcode of next instruction
         opcode opc = config->vmm->read(ip_reg.get_value(), config->opc_sz);
 
+        //ilegal instruction
         if(config->ops_symtable.count(opc) == 0)
             return; //error
 
+        //retrive op info
         auto& op = config->ops_symtable[opc];
         
+        //read args for an operation
         std::vector<byte> args = config->vmm->read_bytes(ip_reg.get_value() + config->opc_sz, op.args_sz_bytes);
         ip_reg.set_value(ip_reg.get_value() + config->opc_sz + op.args_sz_bytes);
 
